f52a8c4df2f0a060c6242b11aace74bb6e6971d9
nick <niksa.derek@gmail.com>
2025-03-14 01:41:34 +0100

    Add files via upload

---
+++ build/precache-manifest.b066be58e170703cc4939ce54965d5da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb5c93208fe8db00b652410f35909f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5292100d7ab14f9cb86f",
+    "url": "/static/css/main.b32063e2.chunk.css"
+  },
+  {
+    "revision": "f8e6c6048b344a450132",
+    "url": "/static/js/2.145ad681.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.145ad681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5292100d7ab14f9cb86f",
+    "url": "/static/js/main.c3acdd5e.chunk.js"
+  },
+  {
+    "revision": "fc7fd6286bacaba85419",
+    "url": "/static/js/runtime-main.f6e6c18f.js"
+  }
+]);
