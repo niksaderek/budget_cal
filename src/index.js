6c27bc5112c41b963e07652d972bcaf2f0007985
nick <niksa.derek@gmail.com>
2025-03-17 17:53:25 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import BudgetCalculator from './BudgetCalculator'
+import App from './App'
 import './index.css'
+import './tailwind.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-    <div className="container mx-auto p-4">
-      <BudgetCalculator />
-    </div>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
